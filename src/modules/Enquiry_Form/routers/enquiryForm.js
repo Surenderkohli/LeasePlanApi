0f8aaa275cac656d9c42001f5572c5aa5b3a200d
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-03-04 18:25:56 +0530

    Add enquiryform api v-2 with nodemailer

--- src/modules/Enquiry_Form/routers/enquiryForm.js
+++ src/modules/Enquiry_Form/routers/enquiryForm.js
@@ -9,7 +9,16 @@ router.post(
      httpHandler(async (req, res) => {
           try {
                const result = await enquiryFormService.addForm(req.body);
-               res.send(result);
+               if (result.receiveUpdates) {
+                    sendEmail(result);
+               }
+               return res
+                    .status(201)
+                    .json({
+                         success: true,
+                         data: result,
+                         message: 'Enquiry created successfully',
+                    });
           } catch (error) {
                res.send({ status: 400, success: false, msg: error.message });
           }
@@ -21,11 +30,37 @@ router.get(
      httpHandler(async (req, res) => {
           try {
                const result = await enquiryFormService.getAllForm();
+
                res.send(result);
           } catch (error) {
                res.send({ status: 400, success: false, msg: error.message });
           }
      })
 );
+
+const sendEmail = (enquiry) => {
+     const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+               user: 'your-email@gmail.com',
+               pass: 'your-password',
+          },
+     });
+
+     const mailOptions = {
+          from: 'your-email@gmail.com',
+          to: enquiry.email,
+          subject: 'Thank you for your enquiry',
+          text: 'Thank you for your enquiry. We will get back to you as soon as possible.',
+     };
+
+     transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+               console.log(err);
+          } else {
+               console.log('Email sent: ' + info.response);
+          }
+     });
+};
 
 export default router;

6dc04d0981b273b04f9adfd67785ec0e45db6691
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-03-12 11:42:06 +0530

    Add file upload for car-details to cloudinary

--- src/modules/Inventory/services/carDetails.js
+++ src/modules/Inventory/services/carDetails.js
@@ -100,17 +100,11 @@ const getAllCar = async (
      }
 };
 
-const addNewCar = async (data, reqfile) => {
+const addNewCar = async (data, carImage) => {
      try {
-          let carImage = [];
-          if (reqfile) {
-               carImage = reqfile.map((img) => {
-                    return img.filename;
-               });
-          }
           const response = await carDetailModel.create({
                ...data,
-               img: carImage,
+               image: carImage,
           });
           return response;
      } catch (error) {

b0539379892e39f60f04bdb8b3a0c61fa93b9cd1
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-05-02 11:09:39 +0530

    Remove commented code v-4

--- src/modules/Inventory/services/carDetails.js
+++ src/modules/Inventory/services/carDetails.js
@@ -128,9 +128,6 @@ const getAllCar = async (
                {
                     $unwind: '$carSeries',
                },
-               // {
-               //      $unwind: '$leaseType',
-               // },
                {
                     $unwind: '$offers',
                },

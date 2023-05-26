e37eb7a4bcd2faaf691ecc7e67f498c1d0088352
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-05-26 23:18:00 +0530

    Add api to filter series of cars based on term lease and carbrand_id v-4

--- src/modules/Inventory/models/carSeries.js
+++ src/modules/Inventory/models/carSeries.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const carBrandSchema = new mongoose.Schema(
+const carSeriesSchema = new mongoose.Schema(
      {
           carBrand_id: {
                type: mongoose.Schema.ObjectId,
@@ -22,4 +22,4 @@ const carBrandSchema = new mongoose.Schema(
      { timestamps: true }
 );
 
-export default mongoose.model('carSeries', carBrandSchema);
+export default mongoose.model('carSeries', carSeriesSchema);
